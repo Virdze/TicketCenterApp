4002a99d5e0d3aee508ffff38dfb60001b539bbc
virdze <joao.andre.freitas@hotmail.com>
2023-07-01 09:33:12 +0100

    Added lombok annotations

---
+++ demo/src/main/java/vidze/demo/Forms/Requests/Request.java
@@ -0,0 +1,13 @@
+package vidze.demo.Forms.Requests;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Request {
+    
+}
