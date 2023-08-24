ec2454a2aa944cb907de661b7179e823846a644f
virdze <joao.andre.freitas@hotmail.com>
2023-08-24 21:47:37 +0100

    JWT config 1/2 done

---
+++ demo/src/main/java/vidze/demo/Configs/JWTService.java
@@ -0,0 +1,11 @@
+package vidze.demo.Configs;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+    public String extractUsername(String token){
+        return null;
+
+    }
+}
--- demo/src/main/java/vidze/demo/Models/User.java
+++ demo/src/main/java/vidze/demo/Models/User.java
@@ -1,7 +1,12 @@
 package vidze.demo.Models;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -15,7 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "user")
-public class User implements Serializable{
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -37,7 +42,7 @@ public class User implements Serializable{
     @Column(name = "phone")
     private int phone;
 
-    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @Column(name = "language")
@@ -45,5 +50,42 @@ public class User implements Serializable{
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Ad> ads;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    } 
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+
     
 }
